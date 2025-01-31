079087172aaf837367cb091ece6a1b2e47812af8
rgjc12 <rohitgupta35757@gmail.com>
2025-01-31 16:44:26 +0530

    Done Except Cursor

--- Frontend/src/pages/Paceorder/Placeorder.jsx
+++ Frontend/src/pages/Paceorder/Placeorder.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Placeorder.css";
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
@@ -7,13 +7,19 @@ import Checkout from './Checkout';
 import { Routes, Route, NavLink,Navigate } from 'react-router-dom';
 import Lenis from "lenis";
 import {useLocation } from "react-router-dom";
+import { asyncgetCart } from '../../store/actions/asyncCart';
+import { useSelector ,useDispatch} from 'react-redux';
+import { setTotalNumberOfItems } from '../../store/Reducers/totalNumberOfItems';
 
 function Placeorder() {
   const lenis = new Lenis();
   const location = useLocation();
+  const dispatch=useDispatch();
+  const userId=useSelector(state=>state.userId.userId);
   useEffect(() => {
     function raf(time) {
       lenis.raf(time);
+
       requestAnimationFrame(raf);
     }
     requestAnimationFrame(raf);
@@ -28,11 +34,28 @@ function Placeorder() {
 
 
 
+  useEffect(()=>{
+    dispatch(asyncgetCart(userId));
+  },[userId])
+
+  let [totalnumberofitems,settotalnumberofitems]=useState(0);
+  const cart = useSelector(state => state.cart.cart);
+  
+
+   useEffect(() => {
+    if (Array.isArray(cart)) {
+      let total = cart.reduce((acc, item) => acc + item.quantity, 0);
+      settotalnumberofitems(total);
+      dispatch(setTotalNumberOfItems(total));
+      localStorage.setItem('totalNumberOfItems', total);
+    }
+  }, [cart]);
+
 
 
   return (
     <>
-      <Navbar num={3} totalnumberofitems={0} />
+      <Navbar num={3} totalnumberofitems={totalnumberofitems} />
 
       <div id="placeorderbuttons">
         <NavLink to="/placeorder/orders">
